8072c3a39b3c9ba6a8e4decf3606345100daad3e
atapia27 <tapia6176@gmail.com>
2022-03-03 01:06:35 -0800

    Flappi Bird Parody

---
+++ Assets/Bird_Zoomies.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_Zoomies : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //Jump
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+
+
+}
---
+++ Assets/Pipe_Movement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe_Movement : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
